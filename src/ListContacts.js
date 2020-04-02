06f11957e68f27de3ac97369eabc4d29dfdd06a6
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-04-02 00:58:41 -0300

    Add button to create contacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -23,7 +23,7 @@ class ListContacts extends Component {
 
   render() {
     const { query } = this.state
-    const { contacts, onDeleteContact } = this.props
+    const { contacts, onDeleteContact, onNavigate } = this.props
 
     const showingContacts =
       query === ''
@@ -42,6 +42,9 @@ class ListContacts extends Component {
             value={query}
             onChange={event => this.updateQuery(event.target.value)}
           />
+          <a href="#create" onClick={onNavigate} className="add-contact">
+            Add Contact
+          </a>
         </div>
         <div>
           {showingContacts.length !== contacts.length && (
